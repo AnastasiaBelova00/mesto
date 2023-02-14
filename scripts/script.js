9da2290bd17e2ddd56a6f8172bebb9e9076cffbf
Anastasia Belova <nastasya.belowa@yandex.ru>
2023-02-14 23:29:15 +0300

    feat: js

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+const popup = document.querySelector(".popup");
+const editProfileButton = document.querySelector(".profile__button_act_edit");
+const closePopupButton = document.querySelector(".popup__button_act_exit");
+const profileName = document.querySelector(".profile__name");
+const profileDiscription = document.querySelector(".profile__discription");
+const editForm = document.querySelector(".popup__container");
+
+let nameInput = document.querySelector(".popup__input_name");
+let jobInput = document.querySelector(".popup__input_discription");
+
+editProfileButton.addEventListener("click", function () {
+  popup.classList.add("popup_opened");
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileDiscription.textContent;
+});
+
+closePopupButton.addEventListener("click", function () {
+  popup.classList.remove("popup_opened");
+});
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileDiscription.textContent = jobInput.value;
+  popup.classList.remove("popup_opened");
+}
+
+editForm.addEventListener("submit", handleFormSubmit);
